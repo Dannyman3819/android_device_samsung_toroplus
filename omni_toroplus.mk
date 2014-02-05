5f4040c4f9c93ac7fbf2d1feac5de09df3862a96
Dannyman3819 <danielb2cool@gmail.com>
2014-02-04 18:38:16 -0700

    port to omni

--- omni_toroplus.mk
+++ omni_toroplus.mk
@@ -5,18 +5,19 @@ PRODUCT_RELEASE_NAME := GN-CDMA
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/cdma.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/toroplus/aosp_toroplus.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+#$(call inherit-product, vendor/omni/config/nfc_enhanced.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := toroplus
-PRODUCT_NAME := cm_toroplus
+PRODUCT_NAME := omni_toroplus
 PRODUCT_BRAND := Google
 PRODUCT_MODEL := Galaxy Nexus
 PRODUCT_MANUFACTURER := Samsung
